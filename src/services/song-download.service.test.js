275311c73fd6cffba367ff24fc5d42913b765267
oscarvx00 <oscarvicente@usal.es>
2022-12-03 16:51:01 +0100

    Implemented test content

--- src/services/song-download.service.test.js
+++ src/services/song-download.service.test.js
@@ -1,7 +1,23 @@
-import { getSongDownloads, downloadSong } from './song-download.service'
+const functions = require('./song-download.service')
 
 
+const TEST_SONG_DOWNLOADS_DATA = [
+    { name: "dasd", downloadId: "90db2112-d673-4d7b-9b69-791ab2b96cc9", ready: false },
+    { name: "oliver heldens posidon", downloadId: "0838039b-c81e-4c7a-bd01-d4327d7745d4", ready: true }
+]
 
-it('test', () => {
-    expect(true).toBe(true)
+jest.mock('axios')
+
+
+test('getSongDownloads', async () => {
+    axios.get.mockIMplementation(() => Promise.resolve(
+        {
+            data: TEST_SONG_DOWNLOADS_DATA
+        }
+    ))
+
+    const result = functions.getSongDownloads()
+
+    expect(result).toEqual(TEST_SONG_DOWNLOADS_DATA)
 })
+
